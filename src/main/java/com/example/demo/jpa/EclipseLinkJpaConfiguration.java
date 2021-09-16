44b4876c718a2c681dc2394b82357785672ffb7b
Tobias Hein <tobi.hein@gmail.com>
2021-09-16 18:12:59 +0200

    Use EclipseLink instead of hibernate

---
+++ src/main/java/com/example/demo/jpa/EclipseLinkJpaConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.demo.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+import org.eclipse.persistence.logging.SessionLog;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+@Configuration
+public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
+
+	protected EclipseLinkJpaConfiguration(DataSource dataSource, JpaProperties properties,
+			ObjectProvider<JtaTransactionManager> jtaTransactionManager) {
+		super(dataSource, properties, jtaTransactionManager);
+	}
+
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		return new EclipseLinkJpaVendorAdapter();
+	}
+
+	protected Map<String, Object> getVendorProperties() {
+		Map<String, Object> map = new HashMap<>();
+		map.put(PersistenceUnitProperties.WEAVING, "false");
+		map.put(PersistenceUnitProperties.LOGGING_LEVEL, SessionLog.FINER_LABEL);
+		map.put(PersistenceUnitProperties.DDL_GENERATION, PersistenceUnitProperties.DROP_AND_CREATE);
+		map.put(PersistenceUnitProperties.DDL_GENERATION_MODE, "database");
+		return map;
+	}
+}
